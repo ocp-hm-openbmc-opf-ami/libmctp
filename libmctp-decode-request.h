ba5d7cc51d776b2cb990bf03e3c139749831c88d
dagraw1x <deepakx.agrawal@intel.com>
2022-12-01 18:25:49 +0530

    Update libmctp-decode-request.h

--- libmctp-decode-request.h
+++ libmctp-decode-request.h
@@ -6,22 +6,64 @@
 extern "C" {
 #endif
 
+/** @brief Decode function for request structure
+ *
+ *  @param[in] request - Request structure to be decoded
+ *  @param[in] length - Length of request structure
+ *  @param[out] ctrl_hdr - header for request structure 
+ *			   definition: {uint8_t ic_msg_type;uint8_t rq_dgram_inst;
+ *			   uint8_t command_code;}
+ *  @param[out] target_eid - target eid of resolve eid command
+ *  @return decode enum type which tells error or success
+ */
 decode_rc mctp_decode_resolve_eid_req(const struct mctp_msg *request,
 				      const size_t length,
 				      struct mctp_ctrl_msg_hdr *ctrl_hdr,
 				      uint8_t *target_eid);
 
+/** @brief Decode function for request structure
+ *
+ *  @param[in] request - Request structure to be decoded
+ *  @param[in] length - Length of request structure
+ *  @param[out] ctrl_hdr - header for request structure 
+ *			   definition: {uint8_t ic_msg_type;uint8_t rq_dgram_inst;
+ *			   uint8_t command_code;}
+ *  @param[out] op - operation field for allocate eid command
+ *  @param[out] eid_pool_size - eid_pool_size field for allocate eid command
+ *  @param[out] first_eid - first_eid field for allocate eid command
+ *  @return decode enum type which tells error or success
+ */
 decode_rc mctp_decode_allocate_endpoint_id_req(
 	const struct mctp_msg *request, const size_t length,
 	struct mctp_ctrl_msg_hdr *ctrl_hdr,
 	mctp_ctrl_cmd_allocate_eids_req_op *op, uint8_t *eid_pool_size,
 	uint8_t *first_eid);
 
+/** @brief Decode function for request structure
+ *
+ *  @param[in] request - Request structure to be decoded
+ *  @param[in] length - Length of request structure
+ *  @param[out] ctrl_hdr - header for request structure 
+ *			   definition: {uint8_t ic_msg_type;uint8_t rq_dgram_inst;
+ *			   uint8_t command_code;}
+ *  @param[out] op - operation field for set eid command
+ *  @param[out] eid - eid field for set eid command
+ *  @return decode enum type which tells error or success
+ */
 decode_rc mctp_decode_set_eid_req(const struct mctp_msg *request,
 				  const size_t length,
 				  struct mctp_ctrl_msg_hdr *ctrl_hdr,
 				  mctp_ctrl_cmd_set_eid_op *op, uint8_t *eid);
 
+/** @brief Decode function for request structure
+ *
+ *  @param[in] request - Request structure to be decoded
+ *  @param[in] length - Length of request structure
+ *  @param[out] ctrl_hdr - header for request structure 
+ *			   definition: {uint8_t ic_msg_type;uint8_t rq_dgram_inst;
+ *			   uint8_t command_code;}
+ *  @return decode enum type which tells error or success
+ */
 decode_rc mctp_decode_get_uuid_req(const struct mctp_msg *request,
 				   const size_t length,
 				   struct mctp_ctrl_msg_hdr *ctrl_hdr);
